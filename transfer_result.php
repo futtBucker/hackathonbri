bfc75baf251a14e239e3f819c1971f934203d2c5
SadharSoft <superbro.ardyan@gmail.com>
2016-10-01 05:04:23 +0700

    2nd commit

--- transfer_result.php
+++ transfer_result.php
@@ -2,9 +2,9 @@
 
 include_once './WS.php';
 
-$pengirim = '087839183232';
-$penerima = '087839182323';
-$pin = '253573';
+$pengirim = '087839182323';
+$penerima = '087839183232';
+$pin = '244401';
 $nominal = $_POST['grandtotal'];
 
 $param = array('nohandphonePengirim' => $pengirim, 'nohandphonePenerima' => $penerima, 'pin' => $pin, 'nominal' => $nominal);
@@ -32,46 +32,7 @@ foreach ($rsl as $key => $value) {
         <title>Hasil Transfer</title>
 
 
-        <!-- Bootstrap Core CSS -->
-        <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- MetisMenu CSS -->
-        <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
-        <link href="dist/css/sb-admin-2.css" rel="stylesheet">
-
-        <!-- Custom Fonts -->
-        <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-        <!--<script src="http://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>-->
-        <script src="misc/jquery-3.1.1.min.js"></script>
-        <!--<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">-->
-        <link rel="stylesheet" type="text/css" href="misc/jquery.dataTables.min.css">
-<!--        <script src="http://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>-->
-        <script src="misc/jquery.dataTables.min.js"></script>
-
-        <!-- Bootstrap Core JavaScript -->
-        <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-        <!-- Metis Menu Plugin JavaScript -->
-        <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-        <!-- Custom Theme JavaScript -->
-        <script src="dist/js/sb-admin-2.js"></script>
-
-        <!--<script type="text/javascript" src='http://cdn.datatables.net/plug-ins/1.10.12/api/sum().js'></script>-->
-        <script type="text/javascript" src='misc/sum().js'></script>
-
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js"></script>
-
-        <!-- (Optional) Latest compiled and minified JavaScript translation files -->
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/i18n/defaults-*.min.js"></script>
-
+        <?php include_once './resource.php'; ?>
 
     </head>
 
